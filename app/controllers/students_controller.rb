81dd87bb49f986787979816ef253a9a053294c6a
gak92 <44802922+gak92@users.noreply.github.com>
2022-10-31 19:54:32 +0500

    Create student controller

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,25 @@
+class StudentsController < ApplicationController
+
+  def index
+    @students = Student.all
+  end
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.new(
+      first_name: params[:student][:first_name],
+      last_name: params[:student][:last_name],
+      email: params[:student][:email]
+    )
+
+    if @student.save
+      redirect_to students_path
+    else
+      render :new
+    end
+  end
+
+end
