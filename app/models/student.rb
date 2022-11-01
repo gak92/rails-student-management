e82e7710ba1de89de253ad047a40191a097b03aa
gak92 <44802922+gak92@users.noreply.github.com>
2022-11-01 22:11:41 +0500

    Update validations

--- app/models/student.rb
+++ app/models/student.rb
@@ -5,10 +5,24 @@ class Student < ApplicationRecord
   has_many :projects, through: :student_projects
 
   validates :first_name, :last_name, :email, presence: true
+  validates :email, uniqueness: true
+  validates :first_name, :last_name, length: {minimum:2, maximum:50}
+  validates :first_name, :last_name, format: { with: /\A[a-zA-z]+\z/, message: 'Only letters allowed' }
+  validate :validate_student_age
 
   # before_create :display_greetings
 
   # def display_greetings
   #   puts "Hello, your executed before action callbacks"
   # end
+
+  def validate_student_age
+    if self.date_of_birth.present?
+      age = Date.today.year - self.date_of_birth.year
+      if age < 15
+        errors.add(:age, 'Please provide valid date of birth, Age must be greater than 15')
+      end
+    end
+  end
+  
 end
